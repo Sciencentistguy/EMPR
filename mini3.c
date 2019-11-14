fa9a3f463f849824b5ef70f7507da53b4eab64e0
Jamie Quigley <jamie.e.quigley@gmail.com>
2019-11-14 17:49:25 +0000

    Revert "it doesn't work"
    This reverts commit 90cb02182c2bde9bdb08b12df7f1f0f951d01117.

--- mini3.c
+++ mini3.c
@@ -8,8 +8,6 @@
 #include <lpc17xx_dac.h>
 #include <lpc17xx_timer.h>
 #include <lpc17xx_gpdma.h>
-#include <lpc17xx_nvic.h>
-#include <lpc17xx_gpio.h>
 
 #include "libs/serial.h"
 #include "libs/led.h"
@@ -23,25 +21,6 @@
 
 
 char buf[128];
-volatile char key = ' ';
-volatile char prevKey = ' ';
-
-void IRQInit() {
-    LPC_PINCON->PINSEL4 |= 0x4000000; //
-    LPC_PINCON->PINSEL0 &= ~(3 << 20); //
-    LPC_SC->EXTMODE = 1 << 3;
-    LPC_SC->EXTPOLAR = 1 << 3;
-    LPC_GPIO0->FIODIR &= ~(1 << 10); //
-    LPC_GPIOINT->IO0IntClr = (1 << 10);
-    LPC_GPIOINT->IO0IntEnF |= (1 << 10); //
-    LPC_SC->EXTINT = 1 << 3; //Clear Pending Interrupts
-    //key = ' ';
-    //buttonpress = 0;
-    //NVIC_SetPriority(EINT3_IRQn, 0x00);
-    GPIO_IntCmd(0, 1 << 23, 1);
-    NVIC_EnableIRQ(EINT3_IRQn);
-    __enable_irq();
-}
 
 
 
@@ -49,49 +28,30 @@ int main() {
     ADC_InitFunc();
     SERIAL_Init();
     KEYPAD_Init();
-    IRQInit();
     DAC_InitFunc();
-    //while (1) {
-    //    zero(buf);
-    //    sprintf(buf, "Current voltage: %fV\r\n", ADC_GetVoltage());
-    //    SERIAL_WriteBuf(buf, sizeof(buf));
-    //    if (KEYPAD_GetKeyPressed() == 'B') {
-    //        break;
-    //    }
-    //}
-    //DAC_SineWave(3000, 2.5);
-    //while (KEYPAD_GetKeyPressed() != 'A')
-    //    (void) 0;
-    //DAC_SineWave(4000, 1.5);
-    //zero(buf);
-    SERIAL_WriteString("go\r\n");
+
     while (1) {
-        sprintf(buf, "%c\r\n", key);
+        zero(buf);
+        sprintf(buf, "Current voltage: %fV\r\n", ADC_GetVoltage());
         SERIAL_WriteBuf(buf, sizeof(buf));
-    }
-}
 
-void EINT3_IRQHandler(void) {
-
-    key = KEYPAD_GetKeyPressed();
-
-    if (key != prevKey && key != ' ') {
-        prevKey = key;
-
-    } else if (key == ' ') {
-        prevKey = ' ';
+        if (KEYPAD_GetKeyPressed() == 'B') {
+            break;
+        }
     }
 
-    LED_WriteInt(15);
-    GPIO_ClearInt(1, 1<<28);
+    DAC_SineWave(3000, 2.5);
+
+    while (KEYPAD_GetKeyPressed() != 'A')
+        (void) 0;
+
+    DAC_SineWave(4000, 1.5);
+
+    for (;;);
 }
 
-//void EINT3_IRQHandler() {
-//    LPC_SC->EXTINT = 1 << 3;
-//    LPC_GPIOINT->IO0IntClr = (1 << 10);
-//    LED_WriteInt(15);
-//    SERIAL_WriteString("I'mHERE\r\n");
-//}
+void EINT3_IRQHandler();
+
 
 void SysTick_Handler() {
     //stub
